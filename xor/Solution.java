10a61f98107e77028d13dd9c79166ec6d6d8f32e
shinigoyal <58323019+shinigoyal@users.noreply.github.com>
2024-09-12 16:31:31 +0400

    xor problem misc

---
+++ xor/Solution.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    // Complete the maxXor function below.
+    static int[] maxXor(int[] arr, int[] queries) {
+        // solve here
+        
+        int[] result = new int[queries.length];
+        
+        for(int i = 0 ; i<queries.length; i++){
+            int max = 0;
+            for(int j = 0; j<arr.length; j++){
+                int xor = queries[i]^ arr[j];
+                if(xor>max){
+                    max = xor;
+                }
+            }
+            result[i] = max;
+        }
+        
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int m = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] queries = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            int queriesItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            queries[i] = queriesItem;
+        }
+
+        int[] result = maxXor(arr, queries);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
