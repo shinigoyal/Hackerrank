e2efe1c754011cf79e558fab3541e5374af82728
shinigoyal <58323019+shinigoyal@users.noreply.github.com>
2024-09-20 17:02:28 +0400

    alternating characters

---
+++ alternating characters/Solution.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.stream.*;
+
+class Result {
+
+
+    public static int alternatingCharacters(String s) {
+    int count =0;
+    int len = s.length();
+    for(int i=0; i+1<len && i<len; i++){
+        if(s.charAt(i) == s.charAt(i+1)){
+            count++;
+        }
+    }
+    return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.alternatingCharacters(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
